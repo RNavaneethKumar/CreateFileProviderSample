b28dcb0309f9a2afbb7cb71e518c7736bced5b15
RNavaneethKumar <rnavaneethkumar@gmail.com>
2022-07-20 23:26:06 +0530

    Initial Checkin
    Checking in sample code for reproducing exception

---
+++ SampleApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.FileProviders;
+
+namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void ContentPage_Loaded(object sender, EventArgs e)
+    {
+        CustomBlazorWebView customBlazorWebView = new CustomBlazorWebView();
+        BlazorWebView webView = customBlazorWebView.CreateWebViewControl(FileSystem.AppDataDirectory);
+    }
+}
+
+public class CustomBlazorWebView : BlazorWebView
+{
+    public override IFileProvider CreateFileProvider(string contentRootDir)
+    {
+        var defaultFileProvider = new PhysicalFileProvider(contentRootDir);
+        return defaultFileProvider;
+    }
+
+    public BlazorWebView CreateWebViewControl(string path)
+    {
+        BlazorWebView blazorWebView = new BlazorWebView();
+        blazorWebView.CreateFileProvider(path);
+        //blazorWebView.HostPage = "index.html";
+        return blazorWebView;
+    }
+}
